3998ba6a7fb829bf295a464be888218725c5450b
Julien Pervillé <julien.perville@mingalar.fr>
2012-08-26 10:23:02 +0630

    Make install.rb behave better when invoked with missing or invalid arguments.

--- install.rb
+++ install.rb
@@ -16,17 +16,31 @@ ADMIN_EMAIL = ARGV[1] || 'root@localhost'
 # The name of the user account on the remote system, default to "deploy"
 ACCOUNT = ARGV[2] || 'deploy'
 
+unless TARGET && ADMIN_EMAIL && ACCOUNT
+  puts "Usage: bundle exec sprinkle -s #{File.basename(__FILE__)} " \
+       "<target_address> <email_address> <remote_account> [<recipe1> ... <recipen>]"
+  puts "Type `bundle exec sprinkle --help for sprinkle options.`"
+  exit(1)
+end
+
 # Recipe list, default to "minimal"
-RECIPES = ARGV[3..-1]
+RECIPES = Array(ARGV[3..-1])
 RECIPES << 'minimal' if RECIPES.empty?
 
-# Load the specified recipes
-recipe_dir = File.join(File.dirname(__FILE__), 'recipes')
-valid_recipes = Dir.glob(File.join(recipe_dir, '*.rb')).collect { |e| File.basename(e, '.rb') }
-RECIPES.each do |recipe|
-  unless valid_recipes.include?(recipe)
-    raise ArgumentError, "Invalid recipe #{recipe.inspect}, not in #{valid_recipes.inspect}"
+# aborts if any invalid recipes is found.
+def barf_on_invalid_recipes(recipes, recipe_dir)
+  valid_recipes = Dir.glob(File.join(recipe_dir, '*.rb')).collect { |e| File.basename(e, '.rb') }
+  invalid_recipes = recipes - valid_recipes
+  unless invalid_recipes.empty?
+    puts "Invalid recipes found: #{invalid_recipes.join(', ')}. Valid recipes are: #{valid_recipes.join(', ')}."
+    exit(1)
   end
+end
+
+# load the specified recipes unless invalid recipes present.
+recipe_dir = File.join(File.dirname(__FILE__), 'recipes')
+barf_on_invalid_recipes(RECIPES, recipe_dir)
+RECIPES.each do |recipe|
   # require_relative doesn't work in this context
   require File.join(recipe_dir, recipe)
 end
